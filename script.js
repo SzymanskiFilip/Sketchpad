575868de12c7d75dfff7c71e79958ebf9380c0a8
=Filip Szymanski <=szymanski.fp@gmail.com>
2021-03-20 21:23:03 +0100

    made a function that removes the old grid when chaning the grid with a forEach loop

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 let container = document.getElementById('container');
-let defaultGridSize = 16;
+let changeSizeButton = document.getElementById('change-size-button');
+
+changeSizeButton.addEventListener('click', function(){
+    changeSize();
+});
+
+function removeGridElements(){
+    let items = document.querySelectorAll('.item');
+    items.forEach(element => element.remove());
+}
+
+
+function changeSize(){
+    let size = prompt('Enter the grid size: ');
+    removeGridElements();
+    initializeGrid(size);
+}
 
 
 function initializeGrid(gridSize){
     for(let x = 1; x < gridSize * gridSize + 1; x++){
         let gridElement = document.createElement("div");
         gridElement.className = "item";
-        gridElement.textContent = `${x}`
+        gridElement.textContent = `${x}`;
         
         container.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
         container.style.gridTemplateRows = `repeat(${gridSize}, auto)`;
@@ -14,5 +30,5 @@ function initializeGrid(gridSize){
     }
 }
 
-initializeGrid(3);
+initializeGrid(2);
 
