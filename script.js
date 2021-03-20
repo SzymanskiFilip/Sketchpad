42acc4b6992c6591e2461469375688737ce40baa
=Filip Szymanski <=szymanski.fp@gmail.com>
2021-03-20 19:35:35 +0100

    created a container link and a gridElement with class of item

---
+++ script.js
@@ -0,0 +1,7 @@
+let container = document.getElementById('container');
+let defaultGridSize = 16;
+
+let gridElement = document.createElement("div");
+gridElement.className = "item";
+
+container.appendChild(gridElement);
