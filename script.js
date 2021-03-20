2899a31d73fb8bc181f0b4540d3de5c990f499c1
=Filip Szymanski <=szymanski.fp@gmail.com>
2021-03-20 20:20:57 +0100

    made a loop that adds 16 divs

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let container = document.getElementById('container');
 let defaultGridSize = 16;
 
-let gridElement = document.createElement("div");
-gridElement.className = "item";
 
-container.appendChild(gridElement);
+for(let x = 0; x < defaultGridSize; x++){
+    let gridElement = document.createElement("div");
+    gridElement.className = "item";
+    container.appendChild(gridElement);
+}
