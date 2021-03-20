039a3b431020de6aef666557aa817d12a307624e
=Filip Szymanski <=szymanski.fp@gmail.com>
2021-03-20 20:44:38 +0100

    created a grid with 16*16 elements

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let container = document.getElementById('container');
 let defaultGridSize = 16;
 
-
-for(let x = 0; x < defaultGridSize; x++){
-    let gridElement = document.createElement("div");
-    gridElement.className = "item";
-    container.appendChild(gridElement);
-}
+for(let x = 0; x < defaultGridSize * defaultGridSize; x++){
+        let gridElement = document.createElement("div");
+        gridElement.className = "item";
+        gridElement.textContent = `${x}`
+        
+        container.style.gridTemplateColumns = `repeat(${defaultGridSize}, auto)`;
+        container.style.gridTemplateRows = `repeat(${defaultGridSize}, auto)`;
+        container.appendChild(gridElement);
+    }
